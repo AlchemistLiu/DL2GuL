77585e87bbd23f1d3c9bd06c2b937720a9b19c6b
AlchemistLiu <1249590157@qq.com>
2021-11-09 20:46:20 +0800

    10

--- ch13_cv/anchor.py
+++ ch13_cv/anchor.py
@@ -120,7 +120,7 @@ show_bboxes(fig.axes, boxes[400, 250, :, :] * bbox_scale,
              's=0.75, r=0.5'])
 plt.show()
 
-# 交并比IoU
+# 计算成对的交并比IoU
 def box_iou(boxes1, boxes2):
     # 计算交并比
     # boxes[:, 2] 取所有行的第三列 h
@@ -169,8 +169,50 @@ def box_iou(boxes1, boxes2):
     inter_upperlefts = torch.max(boxes1[:, None, :2], boxes2[:, :2])
     inter_lowerrights = torch.min(boxes1[:, None, :2], boxes2[:, :2])
     # clamp(min=0)限制值非负
+    # 重叠区域inter_areas
     inters = (inter_lowerrights - inter_upperlefts).clamp(min=0)
     # `inter_areas` and `union_areas`的形状: (boxes1的数量, boxes2的数量)
     inter_areas = inters[:, :, 0] * inters[:, :, 1]
     union_areas = areas1[:,None] + areas2 - inter_areas
     return inter_areas / union_areas
+
+# 将真实边界框分给锚框
+# 把矩阵看成一个表格，表格行表示锚框，列表示真实框，根据其中(锚框, 真实框)坐标中存的IoU值
+def assign_anchor_to_bbox(ground_truth, anchors, device, iou_threshold=0.5):
+    # 将最接近的真实边界框分配给锚框。
+    num_anchors, num_gt_boxes = anchors.shape[0], ground_truth.shape[0]
+    # 位于第i行和第j列的元素 x_ij 是锚框i和真实边界框j的IoU
+    # anchors应该是一堆锚框，ground_truth应该是手动标记的真实锚框数量，用广播操作
+    # 计算生成的锚框与每个真实框之间的IoU
+    jaccard = box_iou(anchors, ground_truth)
+    # 对于每个锚框，分配的真实边界框的张量
+    # torch.full((形状), 值):用来return一个确定值的tensor
+    anchors_bbox_map = torch.full((num_anchors,), -1, dtype=torch.long, device=device)
+    # 根据阈值，决定是否分配真实边界框
+    max_ious, indices = torch.max(jaccard, dim=1)
+    # torch.nonzero()  找出tensor中非零的元素的索引
+    anc_i = torch.nonzero(max_ious >= 0.5).reshape(-1)
+    box_j = indices[max_ious >= 0.5]
+    anchors_bbox_map[anc_i] = box_j
+    # 丢弃的行和列
+    col_discard = torch.full((num_anchors, ), -1)
+    row_discard = torch.full((num_gt_boxes, ), -1)
+    for _ in range(num_gt_boxes):
+        # 变换为索引的值
+        max_idx = torch.argmax(jaccard)
+        box_idx = (max_idx % num_gt_boxes).long()
+        anc_idx = (max_idx / num_gt_boxes).long()
+        anchors_bbox_map[anc_idx] = box_idx
+        jaccard[:, box_idx] = col_discard
+        jaccard[anc_idx, :] = row_discard
+    return anchors_bbox_map
+
+# 标记类和偏移
+def offset_boxes(anchors, assigned_bb, eps=1e-6):
+    # 转换锚框的偏移量
+    c_anc = d2l.box_corner_to_center(anchors)
+    c_assigned_bb = d2l.box_corner_to_center(assigned_bb)
+    offset_xy = 10 * (c_assigned_bb[:, :2] - c_anc[:, :2]) / c_anc[:, 2:]
+    offset_wh = 5 * torch.log(eps + c_assigned_bb[:, 2:] / c_anc[:, 2:])
+    offset = torch.cat([offset_xy, offset_wh], axis=1)
+    return offset
